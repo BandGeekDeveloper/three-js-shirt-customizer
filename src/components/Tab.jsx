599a82b11663f5bef7f032b12334d575eaf83cde
Robin Kelsick II <robinkelsick@gmail.com>
2023-08-05 10:12:42 -0400

    need to fix the state for the icons

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { useSnapshot } from "valtio";
 
-const Tab = () => {
-  return <div>Tab</div>;
+import state from "../store";
+
+const Tab = (tab, isFilterTab, isActiveTab, handleClick) => {
+  const snap = useSnapshot(state);
+
+  const activeStyles =
+    isFilterTab && isActiveTab
+      ? { backgroundColor: snap.color, opacity: 0.5 }
+      : { backgroundColor: "transparent", opacity: 1 };
+
+  return (
+    <div
+      key={tab.name}
+      className={`tab-btn ${
+        isFilterTab ? "rounded-full glassmorphism" : "rounded-4"
+      }`}
+      onClick={handleClick}
+      style={activeStyles}
+    >
+      <img src={tab.icon} />
+    </div>
+  );
 };
 
 export default Tab;
